aa94cec3086baa0c3f4417f4664e29549b34ab63
MarcinMichna <marcinxmichna@gmail.com>
2020-04-07 14:52:34 +0200

    basic layouts

---
+++ app/src/main/java/com/sleepingbaby/Active.java
@@ -0,0 +1,16 @@
+package com.sleepingbaby;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Active extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_active);
+    }
+}
--- app/src/main/java/com/sleepingbaby/MainActivity.java
+++ app/src/main/java/com/sleepingbaby/MainActivity.java
@@ -2,7 +2,9 @@ package com.sleepingbaby;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +14,17 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goStart(View view)
+    {
+        Intent intent = new Intent(this, Active.class);
+        startActivity(intent);
+    }
+
+    public void goSettings(View view)
+    {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
     }
 }
